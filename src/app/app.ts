bd71c6409fcae3556e769a2c256a13bdfb06e9ca
prem singh <premrathore654@gmail.com>
2025-08-21 17:35:07 +0530

    fix:errors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Topticker } from './topticker/topticker';
 import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
-
-import { Topticker } from './topticker/topticker';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Topticker ],
+  imports: [RouterOutlet, Topticker, Header],
 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('anguler-project');
-
-  data(){
-    console.log()
-  }
 
 }
