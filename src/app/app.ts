630b4cd861d3f65df6ab99c15a356b69d5c8944e
Rohit <rk268277@gmail.com>
2025-08-21 16:03:48 +0530

    Toptickers created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Topticker } from './topticker/topticker';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Topticker ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('anguler-project');
-  
+
   data(){
     console.log(10)
   }
