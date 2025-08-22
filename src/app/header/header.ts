7d08f8b4c1c680ba1ed4da976d6ca7e8bd8d40e9
riteshvermacodes <riteshverma.codes@gmail.com>
2025-08-22 16:04:52 +0530

    Changes in header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { data } from './types';
 import { HeaderData } from './variants';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
