793f6d53385075a724cd4fa6b95fd22af8c8cc2c
Rohit <rk268277@gmail.com>
2025-08-23 13:12:05 +0530

    ft:clam added in toptickers

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topticker } from './topticker/topticker';
-import { Header } from './header/header';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngleDoubleLeft, faAngleDoubleRight, faCar, faCoffee, faEye, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topticker, Header, FontAwesomeModule],
+  imports: [RouterOutlet, Topticker, FontAwesomeModule],
 
   templateUrl: './app.html',
   styleUrl: './app.css',
