9b0d5a39a28301b27bf7b6d942cb09f75a4333b0
prem singh <premrathore654@gmail.com>
2025-08-25 17:23:42 +0530

    ft:responsive branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,23 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topticker } from './topticker/topticker';
 import { Header } from './header/header';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAngleDoubleLeft, faAngleDoubleRight, faCar, faCoffee, faEye, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topticker, Header, FontAwesomeModule],
+  imports: [RouterOutlet, Topticker, Header],
 
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('anguler-project');
-   fa = faCoffee;
-   d =  faAngleDoubleRight
-   l =  faAngleDoubleLeft
-   e =  faEye
-   c=   faCar
-   h =  faHome
-
 }
