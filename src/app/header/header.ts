6563ffcedf9513a47574982b55a77cd4173686db
riteshvermacodes <riteshverma.codes@gmail.com>
2025-08-27 15:09:35 +0530

    ft: Add icons

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { data } from './types';
-import { HeaderData } from './variants';
+import { data, HeaderDataNew } from './types';
+import { HeaderData, headerDataNew} from './variants';
 import { CommonModule } from '@angular/common';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './header.html',
 })
 export class Header {
   getheaderdata:data=HeaderData;
+  getheaderdatanew:HeaderDataNew=headerDataNew;
 }
