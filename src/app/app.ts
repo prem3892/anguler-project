934b7427e85e658caa2a190a5c2e6fcdffa4e838
Rohit <rk268277@gmail.com>
2025-09-04 14:45:00 +0530

    slider has been created on home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topticker } from './topticker/topticker';
 import { Header } from './header/header';
+import { Home } from './home/home';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Topticker, Header],
+  imports: [RouterOutlet,Topticker, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
