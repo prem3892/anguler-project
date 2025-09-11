e480e7adc66462a72748ea3234f6deb992424891
riteshvermacodes <riteshverma.codes@gmail.com>
2025-09-11 15:40:09 +0530

    ft/productcategories: New component added

--- src/app/productcategories/variants.ts
+++ src/app/productcategories/variants.ts
@@ -1,12 +1,13 @@
 import { data } from "./types";
 
 export const productcategoriesdata: data = {
-    heading: "Explore Our Best-Selling Product Categories",
+    heading1: "Explore Our Best-Selling Product Categories",
+    heading2: "Top Categories",
     card: [
-        {img: 'images/home.gif', desc:"Card 1"},
-        {img: 'images/home.gif', desc:"Card 1"},
-        {img: 'images/home.gif', desc:"Card 1"},
-        {img: 'images/home.gif', desc:"Card 1"},
-    ]
-
+        { img: 'images/Customcovers.png', desc: "Custom Covers" },
+        { img: 'images/Cusioncovers.png', desc: "Cusion Covers" },
+        { img: 'images/Grillcovers.png', desc: "Cusion Covers" },
+        { img: 'images/Patiocovers.png', desc: "Patio Furniture Covers" },
+    ],
+    button: "View More"
 }
