61519bec772364d5b53b7786057baa90a807d4f0
prem singh <premrathore654@gmail.com>
2025-09-16 17:03:34 +0530

    ft/routing explained

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        title:"home page",
+        component:Home
+    },
+
+    {
+        path:"about",
+        title:"about page",
+        component:About
+    },
+    {
+        path:"contact",
+        title:"contact page",
+        component:Contact
+    }
+  
+];
