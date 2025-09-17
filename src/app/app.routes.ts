d41aea3dc83ace12d97ddd8f92639e12e3ef7fbc
prem singh <premrathore654@gmail.com>
2025-09-17 16:12:51 +0530

    developer updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,8 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
-import { Home } from './home/home';
 import { Contact } from './contact/contact';
 
 export const routes: Routes = [
-
-    {
-        path:"",
-        title:"home page",
-        component:Home
-    },
 
     {
         path:"about",
