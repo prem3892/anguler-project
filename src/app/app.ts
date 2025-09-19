cb7ad284bac2eac1cfaa234df08efd86b69942f0
Rohit <rk268277@gmail.com>
2025-09-19 14:58:25 +0530

    routing has been completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,14 @@
-import { HeroBanner } from './heroBanner/heroBanner';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topticker } from './topticker/topticker';
 import { Header } from './header/header';
 import { Footer } from "./footer/footer";
-import { ProductCart } from './product-cart/product-cart';
-import { Productcategories } from './productcategories/productcategories';
-import { SignalComp } from './signal/signal';
+
 import { GoToTop } from './go-to-top/go-to-top';
-import { Hero } from './hero/hero';
-import { ProductSwiper } from './product-swiper/product-swiper';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topticker, Header, Footer, HeroBanner, ProductCart, Productcategories, SignalComp, GoToTop, Hero, ProductSwiper],
+  imports: [RouterOutlet, Topticker, Header, Footer, GoToTop,],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/header/types.ts
+++ src/app/header/types.ts
@@ -1,16 +1,14 @@
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 export interface data {
-    headerImg: string;
-    headerFill: string;
-    icons: IconDefinition[];
-    headerBottom: {
+  headerImg: string;
+  headerFill: string;
+  icons: IconDefinition[];
+  headerBottom: {
     title: string;
     sub?: string[];
   }[];
 }
 
-
-
-export interface HeaderDataNew{
- sub: { label: string; icon: IconDefinition;}[];
+export interface HeaderDataNew {
+  sub: { label: string; icon: IconDefinition; path?: string }[];
 }
